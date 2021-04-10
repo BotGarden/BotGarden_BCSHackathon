9edd2bacd35eefa591de9b0d92003910cacfda01
Eric Kim <erickim195@gmail.com>
2021-04-10 16:53:26 -0700

    clean up sample command

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,10 +1,9 @@
-import Discord, { Client } from 'discord.js';
+import Discord from 'discord.js';
 
 module.exports = {
     name: 'ping',
     description: 'Ping!',
     execute(message: Discord.Message) {
         message.channel.send('Pong.');
-        message.channel.send('Yooo');
     },
 };
