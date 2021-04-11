f20c139ab01c5d652fa4a0e4f2b9c9c0dc5a5971
Eric Kim <erickim195@gmail.com>
2021-04-11 04:51:59 -0700

    added AskTABot

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { AskBCSBot } from "./bots/AskBCSBot/AskBCSBot"
+import { AskTABot } from "./bots/AskTABot/AskTABot";
 
 class Index {
     async run() {
         await new AskBCSBot().run();
+        await new AskTABot().run();
     }
 }
 new Index().run();
