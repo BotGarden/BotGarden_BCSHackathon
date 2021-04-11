f76333838b74b140380cdd3d4284a39aae90ed27
Eric Kim <erickim195@gmail.com>
2021-04-11 04:39:23 -0700

    remove AskTABot for demo

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import { AskBCSBot } from "./bots/AskBCSBot/AskBCSBot"
-import { AskTABot } from "./bots/AskTABot/AskTABot";
 
 class Index {
     async run() {
         await new AskBCSBot().run();
-        await new AskTABot().run();
     }
 }
 new Index().run();
